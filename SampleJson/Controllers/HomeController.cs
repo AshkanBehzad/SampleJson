c847388abe5c67104872a2f0b9fb2c7f1ec8397d
Behzad Chizari <behzad75chizari@gmail.com>
2017-07-09 17:58:59 +0430

    add

---
+++ SampleJson/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleJson.Models;
+
+namespace SampleJson.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: آخئث
+        public ActionResult Index()
+        {
+            return View();
+        }
+        JsonSampleEntities db = new JsonSampleEntities();
+        [HttpPost]
+        public JsonResult GetList()
+        {
+            return Json(db.Test.ToList());
+        }
+
+        public JsonResult Add(Test test)
+        {
+            db.Test.Add(test);
+            db.SaveChanges();
+            return Json(db.Test.ToList());
+        }
+    }
+}
---
+++ SampleJson/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleJson.MvcApplication" Language="C#" %>
